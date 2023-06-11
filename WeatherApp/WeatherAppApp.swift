63ce024a62d81b91d54f2ca11a5670bfc3ab5c33
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-06-11 19:45:56 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Алексей Артамонов on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
